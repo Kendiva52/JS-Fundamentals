0b95f8bb9653d60f1466a7297e03d4170b0d198d
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-05-22 12:00:36 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv.slice('hello');
 if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
