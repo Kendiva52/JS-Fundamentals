7a3589dd1800b388442f503300f7c5857e3d4d50
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-05-22 13:48:49 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,7 @@
 const arg = process.argv[4];
 
 if (arg === undefined) {
-  console.log('No argument');
-} else if (args === defifined) {
-  console.log('HBTN');
-}else {
+  console.log(''HBTN'');
+} else {
   console.log(arg);
 }
