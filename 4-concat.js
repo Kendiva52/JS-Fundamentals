f77a10789a5014ff383f952698b778fe42ef22d3
unknown <okorodudukenile@gmail.com>
2025-05-23 08:44:24 +0100

    Add 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Access the command-line arguments (skip first two elements)
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print the output in the required format
+console.log(`${arg1} is ${arg2}`);
